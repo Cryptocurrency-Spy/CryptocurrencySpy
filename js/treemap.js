f5196140dad2c3b2b12f82490ce2bef8b3f076c8
shay <shay@utah.edu>
2022-11-10 13:06:35 -0700

    treemap

--- js/treemap.js
+++ js/treemap.js
@@ -45,22 +45,56 @@
     //     "d3-zoom": "1.1.0"
     // };
     
-    d3.csv("data/d3.csv").then(data => {
+    // d3.csv("data/d3.csv").then(data => {
+
+    //     let root = d3.stratify()
+    //         .id(d => d.path)
+    //         .parentId(d => {
+    //             return d.path.substring(0, d.path.lastIndexOf("/"));
+    //         })
+    //         (data)
+    //         .sum(d => d.size)
+    //         .sort((a, b) => b.height - a.height || b.value - a.value);
+
+    //     treemap(root);
+
+    d3.csv("data/consolidated_coin_data.csv").then(data => {
+        // data = [...d3.group(data, d => d.Date)
+        _data = data.filter(d => d.Date == "Apr 24, 2019");
+        _data.push({
+            Currency: "a",
+            "Market Cap": "",
+        })
+        _data = _data.map(d => {
+            t = d["Market Cap"].replaceAll(",","");
+            // console.log(t * 1.0);
+            d["Market Cap"] = t * 1.0;
+            return d;
+        })
+        console.log(_data);
 
         let root = d3.stratify()
-            .id(d => d.path)
-            .parentId(d => {
-                return d.path.substring(0, d.path.lastIndexOf("/"));
+            .id(d => {
+                // console.log(d.Currency);
+                return d.Currency;
             })
-            (data)
-            .sum(d => d.size)
-            .sort((a, b) => b.height - a.height || b.value - a.value);
+            .parentId(d => {
+                p = d.Currency == "a"? null: "a";
+                console.log(p);
+                return d.Currency == "a"? null: "a";
+            })
+            (_data)
+            .sum(d => d["Market Cap"])
+            .sort((a,b) => b.height - a.height || b.value - a.value)
+            ;
 
         treemap(root);
+// Currency,Date,Open,High,Low,Close,Volume,Market Cap
+        console.log(root.leaves());
 
         let cell = svg.selectAll("a")
             .data(root.leaves())
-            .enter().append("a")
+            .enter().append("g")
             .attr("target", "_blank")
             // .attr("xlink:href", d => {
             //     let p = d.data.path.split("/");
@@ -68,7 +102,6 @@
             //             + version[p[1]] + "/src/" + p.slice(2).join("/");
             // })
             .attr("transform", d => "translate(" + d.x0 + "," + d.y0 + ")");
-
         //Notice that the fill is dependent on the hierarchy (2 levels up)
         cell.append("rect")
             .attr("id", d => d.id)
@@ -76,28 +109,30 @@
             .attr("height", d => d.y1 - d.y0)
             .attr("fill", d =>  {
                 let a = d.ancestors();
-                return color(a[a.length - 2].id);
+                return color(a[0].id);
             });
 
-        cell.append("clipPath")
-            .attr("id", d => "clip-" + d.id)
-            .append("use")
-            .attr("xlink:href", d => "#" + d.id);
+        // cell.append("clipPath")
+        //     .attr("id", d => "clip-" + d.id)
+        //     .append("use")
+        //     .attr("xlink:href", d => "#" + d.id);
 
-        let label = cell.append("text")
-            .attr("clip-path", d => "url(#clip-" + d.id + ")");
+        // let label = cell.append("text")
+        //     .attr("clip-path", d => "url(#clip-" + d.id + ")");
 
-        label.append("tspan")
-            .attr("x", 4)
-            .attr("y", 13)
-            .text(d => d.data.path.substring(d.data.path.lastIndexOf("/") + 1, d.data.path.lastIndexOf(".")));
+        // label.append("tspan")
+        //     .attr("x", 4)
+        //     .attr("y", 13)
+        //     .text(d => d.id);
 
-        label.append("tspan")
-            .attr("x", 4)
-            .attr("y", 25)
-            .text(d => format(d.value));
+        // label.append("tspan")
+        //     .attr("x", 4)
+        //     .attr("y", 25)
+        //     .text(d => format(d.value));
 
-        cell.append("title")
+        cell.append("text")
+            // .attr("x", d => 0.5 * (d.x1 -d.x0))
+            .attr("y", d => 0.5 * (d.y1 - d.y0))
             .text(d =>  d.id + "\n" + format(d.value));
         
     })
